63a61cd1a6c6fb8e79f0b9e73bc384857704644a
Yaakov <dbrionesmtz@yahoo.com>
2021-12-28 12:37:45 -0600

    trabajando con la version 2 del api

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,14 @@
 <?php
 
-use App\Http\Controllers\Api\V1\PostController;
+use App\Http\Controllers\Api\V1\PostController as PostV1;
+use App\Http\Controllers\Api\V2\PostController as PostV2;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::apiResource('v1/posts', PostController::class)
+//V1
+Route::apiResource('v1/posts', PostV1::class)
+    ->only(['index','show']);
+
+//V2
+Route::apiResource('v2/posts', PostV2::class)
     ->only(['index','show']);
