7adf9459e156ee3c22ee145745ec4d034a4eda80
Yaakov <dbrionesmtz@yahoo.com>
2021-12-28 13:15:51 -0600

    configurando un middleware para api privada

--- routes/api.php
+++ routes/api.php
@@ -11,4 +11,5 @@ Route::apiResource('v1/posts', PostV1::class)
 
 //V2
 Route::apiResource('v2/posts', PostV2::class)
-    ->only(['index','show']);
+    ->only(['index','show'])
+    ->middleware('auth:sanctum');
