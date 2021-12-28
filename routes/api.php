93d86a3a4b24ff9b73522c2f715b7f4261ea34f5
Yaakov <dbrionesmtz@yahoo.com>
2021-12-28 13:37:12 -0600

    estructura inicial de la autenticacion

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\LoginController;
 use App\Http\Controllers\Api\V1\PostController as PostV1;
 use App\Http\Controllers\Api\V2\PostController as PostV2;
 use Illuminate\Http\Request;
@@ -13,3 +14,5 @@ Route::apiResource('v1/posts', PostV1::class)
 Route::apiResource('v2/posts', PostV2::class)
     ->only(['index','show'])
     ->middleware('auth:sanctum');
+
+Route::post('login',[LoginController::class, 'login']);
