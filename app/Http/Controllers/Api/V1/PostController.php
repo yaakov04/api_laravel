674294ce133f9ab1f12efea871f13199ea0090db
Yaakov <dbrionesmtz@yahoo.com>
2021-12-28 12:02:49 -0600

    creando una clase de recurso para mostrar la respuesta

--- app/Http/Controllers/Api/V1/PostController.php
+++ app/Http/Controllers/Api/V1/PostController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\V1\PostResource;
 use App\Models\Post;
 use Illuminate\Http\Request;
 
@@ -37,7 +38,7 @@ class PostController extends Controller
      */
     public function show(Post $post)
     {
-        return $post;
+        return new PostResource($post);
     }
 
     /**
