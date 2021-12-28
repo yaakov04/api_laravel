e985722def15080029742b55915f83b32cf09405
Yaakov <dbrionesmtz@yahoo.com>
2021-12-28 12:15:40 -0600

    configurando el metodo index del controlador del api

--- app/Http/Controllers/Api/V1/PostController.php
+++ app/Http/Controllers/Api/V1/PostController.php
@@ -16,7 +16,7 @@ class PostController extends Controller
      */
     public function index()
     {
-        //
+        return PostResource::collection(Post::latest()->paginate());
     }
 
     /**
--- routes/api.php
+++ routes/api.php
@@ -4,4 +4,5 @@ use App\Http\Controllers\Api\V1\PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::apiResource('v1/posts', PostController::class)->only('show');
+Route::apiResource('v1/posts', PostController::class)
+    ->only(['index','show']);
